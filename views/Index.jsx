743fc43bd7d70bfc2f81e9b00ed940d4d9c3b4e4
Tyler Puou <tylerpuou@Shalenes-MBP.fios-router.home>
2020-05-26 10:45:19 -0700

    added slider, fixed index page length

--- views/Index.jsx
+++ views/Index.jsx
@@ -41,7 +41,15 @@ class Index extends React.Component {
                         <h4 className='about-title'>About us</h4>
                         <hr></hr>
                         <p className='about-p'>AbovePar is tool made by golfers, for golfers, that helps you keep track of your improvement. Just sign up and start entering your rounds into our tracker. We then calculate your handicap for you and you can see your improvement every round you play, while also having the ability to add any notes of areas that may need some work.</p>
-                        <img className='about-img'src='https://www.stonehamoaks.com/wp-content/uploads/sites/5789/2016/04/background.jpg'/>
+                        <div className='slider'>
+                        <figure>
+                            <img src='https://golfscape.com/blog/wp-content/uploads/2018/11/aphrodite-hills-golf-course.jpg'></img>
+                            <img src='https://www.westhartfordct.gov/images/Leisure/Rockledge/Images/Rockledge_photo_2.jpg'></img>
+                            <img src='https://www.wheretraveler.com/sites/default/files/shadow_creek.jpg'></img>
+                            <img src='https://dh-prod-cdn.azureedge.net/-/media/property/destination-hotels/suncadia-resort/1440x460-interior-heros/suncadia_golf_1440x566-crpd1440x460.jpg?ts=3ec052a1-84aa-4875-85f9-ba2e7afecfbb'></img>
+                            <img src='https://www.stonehamoaks.com/wp-content/uploads/sites/5789/2016/04/background.jpg'/>
+                        </figure>
+                    </div>
                     </div>
                     </div>
                 </div>
